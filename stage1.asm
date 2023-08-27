c7486d15f390f8ced6dc71f1a26634dc961ca5b4
Ross Bamford <roscopeco@gmail.com>
2023-08-28 00:58:43 +0100

    Stack and nice print loop

--- stage1.asm
+++ stage1.asm
@@ -28,21 +28,27 @@ bootsect:
   db  "FAT12"                           ; Filesystem type
   
 start:
-  xor di,di
-  mov si,MSG
-  mov ah,0x0e
+  xor ax,ax               ; Zero ax
+  mov ds,ax               ; Set up data segments...
+  mov es,ax               ; ...
+
+  mov ss,ax               ; Set up stack segment
+  mov sp,0x7c00           ; and use mem below code for stack for now
+
+  mov si,MSG              ; Load message
+  mov ah,0x0e             ; Set up for int 10 function 0x0e (TELETYPE OUTPUT) 
 
 .charloop:
-  lodsb
-  cmp al,0
-  je  .die
-  int 0x10
-  jmp .charloop
+  lodsb                   ; get next char (increments si too)
+  cmp al,0                ; Is is zero?
+  je  .die                ; We're done if so...
+  int 0x10                ; Otherwise, print it
+  jmp .charloop           ; And continue testing...
 
 .die:
-  cli
-  hlt
-  jmp .die
+  cli                     ; No interrupts (permadeath)
+  hlt                     ; Halt (obvs)
+  jmp .die                ; Better safe than sorry :D
 
 MSG db "No bootable device.", 10, 13, 0
 
