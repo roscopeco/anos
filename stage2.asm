c8ff7e126754c5ea4c66c981857cd48f9aec3d7d
Ross Bamford <roscopeco@gmail.com>
2023-09-02 22:15:02 +0100

    Actually enable A20 (keyboard controller) 🥳

--- stage2.asm
+++ stage2.asm
@@ -115,7 +115,7 @@ enable_a20:
 
   call  check_a20                         ; Check the A20 line
 
-  cmp   ax,0                              ; Is AX zero?
+  test  ax,ax                             ; Is AX zero?
   pop   eax                               ; Restore registers
   je    .disabled             
 
@@ -127,11 +127,101 @@ enable_a20:
   mov   byte [0xb8000],'D'                ; Print "D"
   mov   byte [0xb8001],0x4c               ; In color
 
-  ; TODO actually enable, if not already!
+  call  enable_a20_kbd                    ; Try the Keyboard method
+
+  push  eax                               ; Save EAX again
+
+  call  check_a20                         ; Re-check the A20 line
+
+  test  ax,ax                             ; Is AX zero?
+  pop   eax                               ; Restore registers
+  je    .still_disabled             
+  
+  mov   byte [0xb8004],'E'                ; Print "E"
+  mov   byte [0xb8005],0x2a               ; In color
+  ret
+
+.still_disabled:
+  ; TODO just die for now - but we should try BIOS and fast methods too...
+  mov   byte [0xb8004],'X'                ; Print "X"
+  mov   byte [0xb8005],0x4c               ; In color
+
 .die:
   hlt
   jmp   .die                              ; Don't enable, just die for now...
   
+
+; Enable the A20 line with the keyboard controller method. 
+; This is the traditional (i.e. "old") method and _might_ be
+; supported everywhere(?).
+;
+; I wouldn't be surprised some modern computers don't support
+; it though to be fair...
+;
+; Port 0x64 is the keyboard controller command port. Bits:
+;
+; Status Register - PS/2 Controller
+; Bit Meaning
+; 0   Output buffer status (0 = empty, 1 = full) (must be set before attempting to read data from IO port 0x60)
+; 1   Input buffer status (0 = empty, 1 = full) (must be clear before attempting to write data to IO port 0x60 or IO port 0x64)
+; 2   System Flag - Meant to be cleared on reset and set by firmware (via. PS/2 Controller Configuration Byte) if the system passes self tests (POST)
+; 3   Command/data (0 = data written to input buffer is data for PS/2 device, 1 = data written to input buffer is data for PS/2 controller command)
+; 4   Unknown (chipset specific) - May be "keyboard lock" (more likely unused on modern systems)
+; 5   Unknown (chipset specific) - May be "receive time-out" or "second PS/2 port output buffer full"
+; 6   Time-out error (0 = no error, 1 = time-out error)
+; 7   Parity error (0 = no error, 1 = parity error)
+;
+; (from https://stackoverflow.com/questions/21078932/why-test-port-0x64-in-a-bootloader-before-switching-into-protected-mode)
+;
+; Assumes: 32-bit pmode, interrupts disabled
+; Modifies: nothing
+;
+enable_a20_kbd:
+  push  eax                             ; Save registers
+
+  call  .wait_a20_write                 ; Make sure controller is ready for commands...
+  mov   al,0xad                         ; Output 0xad (DISABLE KEYBOARD) to the command port
+  out   0x64,al
+
+  call  .wait_a20_write                 ; Again, wait for it to be ready.
+  mov   al,0xd0                         ; Output 0xd0 (READ OUTPUT) to command port
+  out   0x64,al                       
+
+  call  .wait_a20_read                  ; ... so do the check
+  in    al,0x60                         ; Read output port command into AL
+  push  eax                             ; And stash it away for a bit while setting up a write
+
+  call  .wait_a20_write                      
+  mov   al,0xD1                         ; Output 0xd1 (WRITE OUTPUT) command to command port
+  out   0x64,al
+
+  call  .wait_a20_write                 ; Again, wait until we can write
+  pop   eax                             ; Restore the value we got from the output port above
+  or    al,0x2                          ; Set bit 1 (A20 Gate)
+  out   0x60,al                         ; And write it to the output port
+
+  call  .wait_a20_write                 ; Wait before the final command
+  mov   al,0xae                         ; Output 0xae (ENABLE KEYBOARD) to command port
+  out   0x64,al
+
+  call  .wait_a20_write                 ; Wait one last time to be "sure" it's finished...
+  pop   eax                             ; Restore registers and done
+  ret
+
+.wait_a20_write:
+  in    al,0x64                         ; Read port 0x64
+  test  al,0x02                         ; Test bit 1 (input buffer status)
+  jnz   .wait_a20_write                 ; If it's set, just busywait
+  ret
+
+.wait_a20_read:
+  in    al,0x64                         ; Read port 0x64
+  test  al,0x01                         ; Test bit 0 (output buffer status)
+  jz    .wait_a20_read                  ; If it's clear, just busywait
+  ret
+
+    
+
 
 ; Is the A20 line enabled?
 ;
