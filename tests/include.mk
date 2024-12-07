7f664f362b9ded8ce0f63b487f213dbd8f81cf30
Ross Bamford <roscopeco@gmail.com>
2024-12-07 09:00:09 +0000

    Fix UB in bitmap.c, use UBSan when doing tests (#11)

--- tests/include.mk
+++ tests/include.mk
@@ -1,8 +1,10 @@
 FLOPPY_DEPENDENCIES+=test
 CLEAN_ARTIFACTS+=tests/*.o tests/pmm/*.o tests/vmm/*.o tests/structs/*.o tests/build
-	
+UBSAN_CFLAGS=-fsanitize=undefined -fno-sanitize-recover=all
+TEST_CFLAGS=-g -Ikernel/include -O3 $(UBSAN_CFLAGS)
+
 tests/%.o: tests/%.c tests/munit.h
-	$(CC) -DUNIT_TESTS -g -Ikernel/include -Itests -c -o $@ $<
+	$(CC) -DUNIT_TESTS $(TEST_CFLAGS) -Itests -c -o $@ $<
 
 tests/build:
 	mkdir -p tests/build
@@ -17,31 +19,31 @@ tests/build/structs:
 	mkdir -p tests/build/structs
 
 tests/build/%.o: kernel/%.c tests/build tests/build/pmm tests/build/vmm tests/build/structs
-	$(CC) -DUNIT_TESTS -g -Ikernel/include -c -o $@ $<
+	$(CC) -DUNIT_TESTS $(TEST_CFLAGS) -c -o $@ $<
 
 tests/build/%.o: kernel/%.asm tests/build tests/build/pmm tests/build/vmm tests/build/structs
 	$(ASM) -DUNIT_TESTS -f $(HOST_OBJFORMAT) -Dasm_$(HOST_OBJFORMAT) -F dwarf -g -o $@ $<
 
 tests/build/interrupts: tests/munit.o tests/interrupts.o tests/build/interrupts.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/pmm/bitmap: tests/munit.o tests/pmm/bitmap.o tests/build/pmm/bitmap.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/pmm/pagealloc: tests/munit.o tests/pmm/pagealloc.o tests/build/pmm/pagealloc.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/vmm/vmmapper: tests/munit.o tests/vmm/vmmapper.o tests/build/vmm/vmmapper.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/debugprint: tests/munit.o tests/debugprint.o tests/build/debugprint.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/acpitables: tests/munit.o tests/acpitables.o tests/build/acpitables.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 tests/build/structs/list: tests/munit.o tests/structs/list.o tests/build/structs/list.o
-	$(CC) -o $@ $^
+	$(CC) $(TEST_CFLAGS) -o $@ $^
 
 
 ALL_TESTS=tests/build/interrupts 										\
@@ -53,5 +55,5 @@ ALL_TESTS=tests/build/interrupts 										\
 			tests/build/structs/list
 
 test: $(ALL_TESTS)
-	sh -c 'for test in $^; do $$test; done'
+	sh -c 'for test in $^; do $$test || exit 1; done'
 
