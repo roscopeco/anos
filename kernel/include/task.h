4dc143263cc08292896194135ce2f2f094bde5d8
Ross Bamford <roscopeco@gmail.com>
2025-01-14 21:07:52 +0000

    Add prio / class

--- kernel/include/task.h
+++ kernel/include/task.h
@@ -8,6 +8,7 @@
 #ifndef __ANOS_KERNEL_TASK_H
 #define __ANOS_KERNEL_TASK_H
 
+#include "anos_assert.h"
 #include "process.h"
 #include "structs/list.h"
 #include <stdint.h>
@@ -15,15 +16,22 @@
 #define DEFAULT_TIMESLICE (((uint8_t)20))
 
 typedef enum {
+    TASK_CLASS_REALTIME = 0,
+    TASK_CLASS_HIGH,
+    TASK_CLASS_LOW,
+    TASK_CLASS_IDLE,
+} __attribute__((packed)) TaskClass;
+
+typedef enum {
     TASK_STATE_BLOCKED = 0,
     TASK_STATE_READY,
     TASK_STATE_RUNNING,
-} TaskState;
+} __attribute__((packed)) TaskState;
 
 /*
  * task_switch.asm depends on the exact layout of this!
  */
-typedef struct {
+typedef struct Task {
     ListNode this;  // 16 bytes
     uintptr_t tid;  // 24
     uintptr_t rsp0; // 32
@@ -34,10 +42,14 @@ typedef struct {
     uintptr_t pml4; // 56
 
     uint16_t ts_remain; // 58
-    uint8_t state;      // 59
-    uint8_t res1;       // 60
-    uint32_t res2;      // 64
+    TaskState state;    // 59
+    TaskClass class;    // 60
+    uint8_t prio;       // 61
+    uint8_t res1;       // 62
+    uint16_t res2;      // 64
 } __attribute__((packed)) Task;
+
+static_assert_sizeof(Task, 64);
 
 void task_init(void *tss);
 Task *task_current();
