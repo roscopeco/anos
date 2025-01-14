5151fd22d1eb635e4d0a741b6b884d40a22ddc50
Ross Bamford <roscopeco@gmail.com>
2025-01-14 21:02:40 +0000

    Disable LAPIC debug by default

--- kernel/kdrivers/local_apic.c
+++ kernel/kdrivers/local_apic.c
@@ -17,22 +17,25 @@
 
 void init_local_apic(BIOS_SDTHeader *madt) {
     uint32_t *lapic_addr = ((uint32_t *)(madt + 1));
+#ifdef DEBUG_LAPIC_INIT
     uint32_t *flags = lapic_addr + 1;
     debugstr("LAPIC address (phys : virt) = ");
     printhex32(*lapic_addr, debugchar);
     debugstr(" : 0xffffffa000000000 [");
     printhex32(*flags, debugchar);
     debugstr("]\n");
-
+#endif
     vmm_map_page(KERNEL_HARDWARE_VADDR_BASE, *lapic_addr, PRESENT | WRITE);
 
     uint32_t volatile *lapic = (uint32_t *)(KERNEL_HARDWARE_VADDR_BASE);
 
+#ifdef DEBUG_LAPIC_INIT
     debugstr("LAPIC ID: ");
     printhex32(*REG_LAPIC_ID(lapic), debugchar);
     debugstr("; Version: ");
     printhex32(*REG_LAPIC_VERSION(lapic), debugchar);
     debugstr("\n");
+#endif
 
     // Set spurious interrupt and enable
     *(REG_LAPIC_SPURIOUS(lapic)) = 0x1FF;
