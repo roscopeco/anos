030bd296ea2db1037ec3ef79ab4a3e847ef84837
Ross Bamford <roscopeco@gmail.com>
2025-07-19 10:33:56 +0100

    Tidy up PCI driver

---
+++ servers/pcidrv/enumerate.c
@@ -0,0 +1,91 @@
+/*
+ * pcidrv - PCI enumeration routines
+ * anos - An Operating System
+ *
+ * Copyright (c) 2025 Ross Bamford
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "pci.h"
+
+bool pci_device_exists(const PCIBusDriver *bus_driver, const uint8_t bus,
+                       const uint8_t device, const uint8_t function) {
+    const uint16_t vendor_id =
+            pci_config_read16(bus_driver, bus, device, function, PCI_VENDOR_ID);
+    return vendor_id != 0xFFFF && vendor_id != 0x0000;
+}
+
+void pci_enumerate_function(const PCIBusDriver *bus_driver, const uint8_t bus,
+                            const uint8_t device, const uint8_t function) {
+    if (!pci_device_exists(bus_driver, bus, device, function)) {
+        return;
+    }
+
+    const uint8_t header_type = pci_config_read8(bus_driver, bus, device,
+                                                 function, PCI_HEADER_TYPE);
+#ifdef DEBUG_BUS_DRIVER_ENUM
+    const uint16_t vendor_id =
+            pci_config_read16(bus_driver, bus, device, function, PCI_VENDOR_ID);
+
+    const uint16_t device_id =
+            pci_config_read16(bus_driver, bus, device, function, PCI_DEVICE_ID);
+
+    const uint32_t class_d = pci_config_read32(bus_driver, bus, device,
+                                               function, PCI_CLASS_CODE);
+    const uint8_t class_code = PCI_REG_UU_B(class_d);
+    const uint8_t subclass = PCI_REG_UM_B(class_d);
+    const uint8_t prog_if = PCI_REG_LM_B(class_d);
+
+    printf("PCI %02x:%02x.%x - Vendor: 0x%04x Device: 0x%04x Class: "
+           "%02x.%02x.%02x",
+           bus, device, function, vendor_id, device_id, class_code, subclass,
+           prog_if);
+#endif
+
+    // Check if it's a bridge
+    if ((header_type & 0x7F) == PCI_HEADER_TYPE_BRIDGE) {
+#ifdef DEBUG_BUS_DRIVER_ENUM
+        const uint8_t secondary_bus =
+                pci_config_read8(bus_driver, bus, device, function, 0x19);
+        printf(" [Bridge to bus %02x]", secondary_bus);
+#endif
+
+        // TODO: Recursively enumerate secondary bus
+    }
+
+#ifdef DEBUG_BUS_DRIVER_ENUM
+    printf("\n");
+#endif
+}
+
+void pci_enumerate_device(const PCIBusDriver *bus_driver, const uint8_t bus,
+                          const uint8_t device) {
+    if (!pci_device_exists(bus_driver, bus, device, 0)) {
+        return;
+    }
+
+    const uint8_t header_type =
+            pci_config_read8(bus_driver, bus, device, 0, PCI_HEADER_TYPE);
+
+    // Enumerate function 0
+    pci_enumerate_function(bus_driver, bus, device, 0);
+
+    // If it's a multi-function device, enumerate other functions
+    if (header_type & 0x80) {
+        for (uint8_t function = 1; function < 8; function++) {
+            pci_enumerate_function(bus_driver, bus, device, function);
+        }
+    }
+}
+
+void pci_enumerate_bus(const PCIBusDriver *bus_driver, const uint8_t bus) {
+#ifdef VERY_NOISY_BUS_DRIVER
+    printf("Enumerating PCI bus %02x...\n", bus);
+#endif
+    for (uint8_t device = 0; device < 32; device++) {
+        pci_enumerate_device(bus_driver, bus, device);
+    }
+}
--- servers/pcidrv/main.c
+++ servers/pcidrv/main.c
@@ -5,13 +5,15 @@
  * Copyright (c) 2025 Ross Bamford
  */
 
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <anos/syscalls.h>
+
+#include "enumerate.h"
+#include "pci.h"
 
 #ifndef VERSTR
 #warning Version String not defined (-DVERSTR); Using default
@@ -22,171 +24,7 @@
 #define STRVER(xstrver) XSTRVER(xstrver)
 #define VERSION STRVER(VERSTR)
 
-// PCI Configuration Space offsets
-#define PCI_VENDOR_ID 0x00
-#define PCI_DEVICE_ID 0x02
-#define PCI_COMMAND 0x04
-#define PCI_STATUS 0x06
-#define PCI_REVISION_ID 0x08
-#define PCI_CLASS_CODE 0x09
-#define PCI_HEADER_TYPE 0x0E
-#define PCI_SUBSYSTEM_ID 0x2E
-
-// PCI Header Types
-#define PCI_HEADER_TYPE_NORMAL 0x00
-#define PCI_HEADER_TYPE_BRIDGE 0x01
-#define PCI_HEADER_TYPE_CARDBUS 0x02
-
-// PCI Configuration Space layout
-typedef struct {
-    uint16_t vendor_id;
-    uint16_t device_id;
-    uint16_t command;
-    uint16_t status;
-    uint8_t revision_id;
-    uint8_t prog_if;
-    uint8_t subclass;
-    uint8_t class_code;
-    uint8_t cache_line_size;
-    uint8_t latency_timer;
-    uint8_t header_type;
-    uint8_t bist;
-    uint32_t bar[6];
-    uint32_t cardbus_cis_pointer;
-    uint16_t subsystem_vendor_id;
-    uint16_t subsystem_id;
-    uint32_t expansion_rom_base;
-    uint8_t capabilities_pointer;
-    uint8_t reserved[7];
-    uint8_t interrupt_line;
-    uint8_t interrupt_pin;
-    uint8_t min_grant;
-    uint8_t max_latency;
-} __attribute__((packed)) PCIConfigSpace;
-
-// Bus driver state
-typedef struct {
-    uint64_t ecam_base;
-    uint16_t segment;
-    uint8_t bus_start;
-    uint8_t bus_end;
-    void *mapped_ecam;
-    size_t mapped_size;
-} PCIBusDriver;
-
 static PCIBusDriver bus_driver = {0};
-
-static uint32_t pci_config_read32(const uint8_t bus, const uint8_t device,
-                                  const uint8_t function,
-                                  const uint8_t offset) {
-    if (bus < bus_driver.bus_start || bus > bus_driver.bus_end) {
-        return 0xFFFFFFFF;
-    }
-
-    if (!bus_driver.mapped_ecam) {
-        return 0xFFFFFFFF;
-    }
-
-    const uint64_t device_offset =
-            ((uint64_t)(bus - bus_driver.bus_start) << 20) |
-            ((uint64_t)device << 15) | ((uint64_t)function << 12) | offset;
-
-    const uint32_t *config_addr =
-            (uint32_t *)((uint8_t *)bus_driver.mapped_ecam + device_offset);
-    return *config_addr;
-}
-
-static uint16_t pci_config_read16(const uint8_t bus, const uint8_t device,
-                                  const uint8_t function,
-                                  const uint8_t offset) {
-    const uint32_t dword =
-            pci_config_read32(bus, device, function, offset & ~3);
-    return (uint16_t)(dword >> ((offset & 3) * 8));
-}
-
-static uint8_t pci_config_read8(const uint8_t bus, const uint8_t device,
-                                const uint8_t function, const uint8_t offset) {
-    const uint32_t dword =
-            pci_config_read32(bus, device, function, offset & ~3);
-    return (uint8_t)(dword >> ((offset & 3) * 8));
-}
-
-static bool pci_device_exists(const uint8_t bus, const uint8_t device,
-                              const uint8_t function) {
-    const uint16_t vendor_id =
-            pci_config_read16(bus, device, function, PCI_VENDOR_ID);
-    return vendor_id != 0xFFFF && vendor_id != 0x0000;
-}
-
-static void pci_enumerate_function(const uint8_t bus, const uint8_t device,
-                                   const uint8_t function) {
-    if (!pci_device_exists(bus, device, function)) {
-        return;
-    }
-
-    const uint8_t header_type =
-            pci_config_read8(bus, device, function, PCI_HEADER_TYPE);
-#ifdef DEBUG_BUS_DRIVER_ENUM
-    const uint16_t vendor_id =
-            pci_config_read16(bus, device, function, PCI_VENDOR_ID);
-    const uint16_t device_id =
-            pci_config_read16(bus, device, function, PCI_DEVICE_ID);
-    const uint8_t class_code =
-            pci_config_read8(bus, device, function, PCI_CLASS_CODE);
-    const uint8_t subclass =
-            pci_config_read8(bus, device, function, PCI_CLASS_CODE - 1);
-    const uint8_t prog_if =
-            pci_config_read8(bus, device, function, PCI_CLASS_CODE - 2);
-
-    printf("PCI %02x:%02x.%x - Vendor: 0x%04x Device: 0x%04x Class: "
-           "%02x.%02x.%02x",
-           bus, device, function, vendor_id, device_id, class_code, subclass,
-           prog_if);
-#endif
-
-    // Check if it's a bridge
-    if ((header_type & 0x7F) == PCI_HEADER_TYPE_BRIDGE) {
-#ifdef DEBUG_BUS_DRIVER_ENUM
-        const uint8_t secondary_bus =
-                pci_config_read8(bus, device, function, 0x19);
-        printf(" [Bridge to bus %02x]", secondary_bus);
-#endif
-
-        // TODO: Recursively enumerate secondary bus
-    }
-
-#ifdef DEBUG_BUS_DRIVER_ENUM
-    printf("\n");
-#endif
-}
-
-static void pci_enumerate_device(const uint8_t bus, const uint8_t device) {
-    if (!pci_device_exists(bus, device, 0)) {
-        return;
-    }
-
-    const uint8_t header_type =
-            pci_config_read8(bus, device, 0, PCI_HEADER_TYPE);
-
-    // Enumerate function 0
-    pci_enumerate_function(bus, device, 0);
-
-    // If it's a multi-function device, enumerate other functions
-    if (header_type & 0x80) {
-        for (uint8_t function = 1; function < 8; function++) {
-            pci_enumerate_function(bus, device, function);
-        }
-    }
-}
-
-static void pci_enumerate_bus(const uint8_t bus) {
-#ifdef VERY_NOISY_BUS_DRIVER
-    printf("Enumerating PCI bus %02x...\n", bus);
-#endif
-    for (uint8_t device = 0; device < 32; device++) {
-        pci_enumerate_device(bus, device);
-    }
-}
 
 static int pci_initialize_driver(uint64_t ecam_base, const uint16_t segment,
                                  const uint8_t bus_start,
@@ -235,7 +73,7 @@ static void pci_enumerate_all_buses(void) {
     //      should only have one root bus, can recursively scan from there...
     for (uint16_t bus = bus_driver.bus_start; bus <= bus_driver.bus_end;
          bus++) {
-        pci_enumerate_bus(bus);
+        pci_enumerate_bus(&bus_driver, bus);
     }
 }
 
@@ -253,25 +91,21 @@ int main(const int argc, char **argv) {
         return 1;
     }
 
-    // Parse arguments
-    const uint64_t ecam_base = strtoull(argv[1], NULL, 16);
-    const uint16_t segment = (uint16_t)strtoul(argv[2], NULL, 10);
-    const uint8_t bus_start = (uint8_t)strtoul(argv[3], NULL, 10);
-    const uint8_t bus_end = (uint8_t)strtoul(argv[4], NULL, 10);
+    const uint64_t ecam_base = strtoull(argv[1], nullptr, 16);
+    const uint16_t segment = (uint16_t)strtoul(argv[2], nullptr, 10);
+    const uint8_t bus_start = (uint8_t)strtoul(argv[3], nullptr, 10);
+    const uint8_t bus_end = (uint8_t)strtoul(argv[4], nullptr, 10);
 
-    // Validate bus range
     if (bus_start > bus_end) {
         printf("Invalid bus range: %u-%u\n", bus_start, bus_end);
         return 1;
     }
 
-    // Initialize the driver
     if (pci_initialize_driver(ecam_base, segment, bus_start, bus_end) != 0) {
         printf("Failed to initialize PCI driver\n");
         return 1;
     }
 
-    // Enumerate all devices on all buses in our range
     pci_enumerate_all_buses();
 
     printf("\nPCI enumeration complete, PCI driver exiting.\n");
