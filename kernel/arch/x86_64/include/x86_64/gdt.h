bd46484b703cea373b03c14b09ef6a9d1748e733
Ross Bamford <roscopeco@gmail.com>
2025-09-14 21:15:49 +0100

    Tidy up low-level GDT code a bit

--- kernel/arch/x86_64/include/x86_64/gdt.h
+++ kernel/arch/x86_64/include/x86_64/gdt.h
@@ -66,14 +66,14 @@ static_assert_sizeof(GDTEntry, ==, 8);
 static_assert_sizeof(GDTSystemEntry, ==, 16);
 
 // Function to get a GDT entry given a GDTR and index
-GDTEntry *get_gdt_entry(GDTR *gdtr, int index);
+GDTEntry *get_gdt_entry(const GDTR *gdtr, int index);
 
 // Update values in a GDT entry. Caller should disable interrupts!
 void init_gdt_entry(GDTEntry *entry, uint32_t base, uint32_t limit,
                     uint8_t access, uint8_t flags_limit_h);
 
 // Get a TSS pointer from a TSS GDT entry
-void *gdt_entry_to_tss(GDTSystemEntry *tss_entry);
+void *gdt_entry_to_tss(const GDTSystemEntry *tss_entry);
 
 // Get a per-CPU TSS pointer
 void *gdt_per_cpu_tss(uint8_t cpu_id);
